aaa99ff663d3e1f383323ef16c64afc5ddaa462f
Un Couteau <5453712-un_couteau@users.noreply.gitlab.com>
2023-04-24 20:12:08 +0500

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.lab1 import *
 
 
 def main():
-    print(number_one())
+    number_one()
     print(number_two(input("Введите название фильма: "), input("Введите название кинотеатра: "), input("Введите время: ")))
     print(number_three(int(input("Введите первое число: ")), int(input("Введите второе число: ")),
                        int(input("Введите третье число: "))))
